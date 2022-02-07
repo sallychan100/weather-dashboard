ba59f7916536027ddb2b9fd74982951fdc052b5a
Sally <sallywing123@gmail.com>
2022-02-06 20:00:00 -0800

    working on displaying

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,26 @@
-var pastCitySearch = []
-
-function cityFromUser (event)  {
+// get input from user 
+function formSubmitHandler (event)  {
     //prevent page from refreshing when submit button is clicked
     event.preventDefault()
     var cityInput = document.getElementById("city-input").value; 
-    currentWeather (cityInput)
+    getLatAndLon (cityInput)
 }
-
-function currentWeather (cityInput) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=4d98bc41af7871594d875bc087999e62")
+// look up city's lat and lon 
+function getLatAndLon (cityInput) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=metric&appid=4d98bc41af7871594d875bc087999e62")
     .then(function (response) {
         if (response.ok) {
           response.json()
           .then(function (data) {
-        console.log(data)
+            console.log(data)
+        var city = data.name;
         var latitude = data.coord.lat;
         var longitude = data.coord.lon;
-        var city = data.name;
-        saveToLocalStorage(city)
-        forecast(latitude,longitude)
+
+        //save to local storage 
+        saveToLocalStorage(city,latitude,longitude)
+
+        weather(latitude,longitude)
         })
     }else {
         window.alert('Search unsuccessful. Please enter a valid city name.') 
@@ -26,19 +28,50 @@ function currentWeather (cityInput) {
     
     })
 }
-
-function forecast (latitude,longitude) {
-    //cnt=5 for 5 days forecast
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&cnt=5&appid=4d98bc41af7871594d875bc087999e62")
+function weather (latitude,longitude) {
+    //cnt=5 for 5 days forecast 
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=metric&cnt=5&appid=4d98bc41af7871594d875bc087999e62")
     .then(response => response.json()) 
     .then(data => {
         console.log(data)
+        var currentTemp = data.current.temp;
+        var currentWind = data.current.weather.wind_speed;
+        var currentHumidity = data.current.humidity;
+        var currentUv= data.current.uvi;
+        displayCurrent (currentTemp,currentWind,currentHumidity,currentUv);
     })
 }
-function saveToLocalStorage (city){
-    pastCitySearch.push(city)
-    localStorage.setItem('name', pastCitySearch)
+
+//Display current date weather 
+var displayCurrent = function (currentTemp,currentWind,currentHumidity,currentUv) {
+    document.getElementById("current-date").innerHTML =$moment().format("MMM Do YY");
+
+    var currentContainer = document.getElementsByClassName('current-weather')
+    currentContainer.appendchild(currentTemp)
 }
 
-document.getElementById("submit-button").addEventListener("click", cityFromUser)
+
+var pastCitySearch = []
+
+// data from current weather function passed on 
+function saveToLocalStorage (city,latitude,longitude){
+    pastCitySearch.push(city)
+    localStorage.setItem('name', pastCitySearch)
+    localStorage.setItem('latitude', latitude)
+    localStorage.setItem('longitude', longitude)
+}
+
+//Displaying past city name 
+// var titleEl = document.createElement('span');
+
+// for (var i = 0; i < pastCitySearch.length; i++) {
+//  var pastEl = document.createElement('a');
+    // issueEl.classList = 'list-item flex-row justify-space-between align-center';
+    // issueEl.setAttribute('href', issues[i].html_url);
+    // issueEl.setAttribute('target', '_blank');
+
+//click on past search and re-search again
+
+
+document.getElementById("submit-button").addEventListener("click", formSubmitHandler)
 
